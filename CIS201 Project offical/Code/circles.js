afff55191a680d70df90f68e05f8ccf6eeaeb6c1
Yomna-J <61107570+Yomna-J@users.noreply.github.com>
2020-04-03 18:35:52 +0300

    Fixing circles bugs
    -Changing images of the circles to pixels.
    -changing section size

--- CIS201 Project offical/Code/circles.js
+++ CIS201 Project offical/Code/circles.js
@@ -4,53 +4,33 @@
  Maruf-Al Bashir Reza
  */
 
-function myFunction(x) {
-  
-  let size = 150;
-  if (x.matches) {
-    // If media query matches
-    size = 100;
+console.log("JavaScript is amazing!");
+$(document).ready(function($) {
+  function animateElements() {
+    $('.progressbar').each(function() {
+      var elementPos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      var percent = $(this).find('.circle').attr('data-percent');
+      var percentage = parseInt(percent, 10) / parseInt(100, 10);
+      var animate = $(this).data('animate');
+      if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+        $(this).data('animate', true);
+        $(this).find('.circle').circleProgress({
+          startAngle: -Math.PI / 2,
+          value: percent / 100,
+          thickness: 4,
+          lincCape:'round',
+          emptyFill:'#d4d4d4',
+          fill: {
+            color: '#1F88E9'
+          },
+          size:140
+        })
+      }
+    });
   }
 
-
-
-    function animateElements() {
-      $(".progressbar").each(function () {
-        var elementPos = $(this).offset().top;
-        var topOfWindow = $(window).scrollTop();
-        var percent = $(this).find(".circle").attr("data-percent");
-        var percentage = parseInt(percent, 10) / parseInt(100, 10);
-        var animate = $(this).data("animate");
-        if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
-          $(this).data("animate", true);
-          $(this)
-            .find(".circle")
-            .circleProgress({
-              startAngle: -Math.PI / 2,
-              value: percent / 100,
-              thickness: 4,
-              lincCape: "round",
-              emptyFill: "#d4d4d4",
-              fill: {
-                color: "#1F88E9",
-              },
-              size: size,
-            });
-          $(this).data("animate", false);
-        }
-      });
-    }
-
-    // Show animated elements
-    animateElements();
-    $(window).scroll(animateElements);
-}
-
-var x = window.matchMedia("(max-width: 750px)");
-myFunction(x);
-
-$(window).on("resize", function (e) {
-  var x = window.matchMedia("(max-width: 750px)");
-  myFunction(x); // Call listener function at run time
+  // Show animated elements
+  animateElements();
+  $(window).scroll(animateElements);
 });
-
