92bfd2aec6771716305a7a9f05bca39686eeb906
Youmna Jaza <yomnayy@hotmail.com>
2022-02-21 21:35:22 +0300

    Created docs

---
+++ docs/formScript.js
@@ -0,0 +1,26 @@
+"use strict";
+$(document).ready(function () {
+    /*------- button with class register -------*/
+    var reg_btn = $('#contact .box-1 .send');
+    
+   
+    reg_btn.click(function (e) {
+        if($('#fullName' ).val() === ''||$('#email').val()===''){
+        swal.fire("Please Fill All the Fields");
+         }
+        else{
+        e.preventDefault();
+        $(this).siblings('.reg').css({
+            'transform': 'translateY(40%) scale(5)',
+            'border-radius': '0',
+            'width': '100%',
+            'height': '100%'
+        }).end();
+        reg_btn.siblings('#contact .box-1 h3:nth-of-type(1)').css({
+            'top': '40%',
+            'z-index': '8',
+        }).end().end();
+        }
+    });
+})
+
